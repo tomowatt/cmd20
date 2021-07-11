d91bc5be0ca314e40ed2e2b17d3db43ca7dd137e
tom <tomowatt@me.com>
2021-07-11 10:13:29 +0100

    Create MVP
    * Using clap to handle CMDLine Arguments although needing to parse values into correct data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::{App, Arg};
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("cmd20")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("dice")
+                .short("d")
+                .long("dice")
+                .default_value("20")
+                .possible_values(&["4", "6", "8", "10", "12", "20", "100"]),
+        )
+        .get_matches();
+
+    let dice: usize = matches.value_of("dice").unwrap().parse().unwrap();
+
+    let mut rng = rand::thread_rng();
+    let roll = rng.gen_range(1, dice + 1);
+
+    //TODO: Add dice templates
+    // let template = match dice {
+    //     100 => r"",
+    //     20 => r"",
+    //     12 => r"",
+    //     10 => r"",
+    //     8 => r"",
+    //     6 => r"",
+    //     4 => r"",
+    //     _ => "@",
+    // };
+
+    println!("{}", roll);
 }
