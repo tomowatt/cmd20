537a31438b38689df82f777753792f911834f7ad
Tom Watt <tom.watt@spongelearning.com>
2021-08-27 15:17:43 +0100

    Refactored application logic into library for modularity

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+
+fn pad(roll: usize, dice: usize) -> String {
+    fn add_padding(roll: String) -> String {
+        let padded: String = match roll.len() {
+            3 => roll,
+            2 => "@ *".replace("@", &roll[..1]).replace("*", &roll[1..]),
+            1 => " @ ".replace("@", &roll),
+            _ => roll,
+        };
+        padded
+    };
+
+    let padded: String = match dice {
+        100 => add_padding(roll.to_string()),
+        10..=99 => add_padding(roll.to_string()),
+        _ => roll.to_string(),
+    };
+    padded
+}
+
+pub fn cmd20(dice: usize) -> String {
+    let mut rng = rand::thread_rng();
+    let roll = rng.gen_range(1, dice + 1);
+
+    let padded_roll = pad(roll, dice);
+
+    let template = match dice {
+        100 => "    ___\n  /     \\\n /       \\\n \\  @  /\n  \\     /\n    ---",
+        20 => "      . \n ¸·___|___`.\n|    / \\    |\n|\\  /@\\  /|\n|_\\/_____\\/_|\n `. \\   / .'\n   ` \\ / '",
+        12 => "  ,'.\n,'   `.\n\\ @ /\n \\___/",
+        10 => "  // \\\\\n //   \\\\\n// @ \\\\\n|/ \\ / \\|\n \\  |  /\n  \\ | /",
+        8 => "  / \\\n / @ \\\n/_____\\\n\\     /\n \\   /\n  \\ /",
+        6 => " -----\n|     |\n|  @  |\n|     |\n -----",
+        4 => "  / \\\n / @ \\\n/_____\\",
+        _ => "@",
+    };
+
+    template.replace("@", &padded_roll)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
 use clap::{App, Arg};
-use rand::Rng;
 
-fn pad(roll: usize, dice: usize) -> String {
-    fn add_padding(roll: String) -> String {
-        let padded: String = match roll.len() {
-            3 => roll,
-            2 => "@ *".replace("@", &roll[..1]).replace("*", &roll[1..]),
-            1 => " @ ".replace("@", &roll),
-            _ => roll,
-        };
-        padded
-    };
-
-    let padded: String = match dice {
-        100 => add_padding(roll.to_string()),
-        10..=99 => add_padding(roll.to_string()),
-        _ => roll.to_string(),
-    };
-    padded
-}
+use cmd20::cmd20;
 
 fn main() {
     let matches = App::new("cmd20")
-        .version("0.7.0")
+        .version("0.8.0")
         .arg(
             Arg::with_name("dice")
                 .short("d")
@@ -34,22 +16,5 @@ fn main() {
 
     let dice: usize = matches.value_of("dice").unwrap().parse().unwrap();
 
-    let mut rng = rand::thread_rng();
-    let roll = rng.gen_range(1, dice + 1);
-
-    let padded_roll = pad(roll, dice);
-
-    //TODO: Add dice templates
-    let template = match dice {
-        100 => "    ___\n  /     \\\n /       \\\n \\  @  /\n  \\     /\n    ---",
-        20 => "      . \n ¸·___|___`.\n|    / \\    |\n|\\  /@\\  /|\n|_\\/_____\\/_|\n `. \\   / .'\n   ` \\ / '",
-        12 => "  ,'.\n,'   `.\n\\ @ /\n \\___/",
-        10 => "  // \\\\\n //   \\\\\n// @ \\\\\n|/ \\ / \\|\n \\  |  /\n  \\ | /",
-        8 => "  / \\\n / @ \\\n/_____\\\n\\     /\n \\   /\n  \\ /",
-        6 => " -----\n|     |\n|  @  |\n|     |\n -----",
-        4 => "  / \\\n / @ \\\n/_____\\",
-        _ => "@",
-    };
-
-    println!("{}", template.replace("@", &padded_roll));
+    println!("{}", cmd20(dice));
 }
